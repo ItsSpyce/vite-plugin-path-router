db10eb7cd66c35abccb6450e1a98585ae6d14167
jpeacox <jcox@jhancock.com>
2022-05-25 16:24:44 -0400

    ok now we switch (I hate git extension)

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -1,19 +1,15 @@
-import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import { PathRoutes } from 'vite-plugin-react-path-router/client';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { PathRoutes } from 'vite-page-router';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Link to="/">Home</Link>
-        <br />
-        <Link to="dynamic-route">Dynamic Route</Link>
-        <br />
-        <Link to="other-dynamic-route">Other Dynamic Route</Link>
-        <PathRoutes>
+        <PathRoutes
+          pages={import.meta.glob('./**/pages/**/*.(t|s)sx')}
+          fallbackLayout={({ children }) => <div id="layout">{children}</div>}
+        >
           <Route
             path="dynamic-route"
             element={<h1>This is a dynamic route!</h1>}
